8f5ff406b00847a76e4caf9c6a2ce648303febfc
Bruno dos Santos Silva <142647702+B4Hive@users.noreply.github.com>
2025-01-28 20:26:06 +0000

    Started entity.java

--- src/main/java/br/ufjf/b4hive/model/entity/Entity.java
+++ src/main/java/br/ufjf/b4hive/model/entity/Entity.java
@@ -1,5 +1,14 @@
 package br.ufjf.b4hive.model.entity;
+import br.ufjf.b4hive.model.field.Coordinate;
+import br.ufjf.b4hive.model.inventory.Inventory;
+import java.util.ArrayList;
 
 public abstract class Entity {
-    
+    private final int id;
+	private final String name;
+	private int hpBase;
+	private float hpCurrent;
+	privte Cooordinate position;
+	private Inventory inventory;
+	private List<Effect> effects;
 }
