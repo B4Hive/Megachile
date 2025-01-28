07d26a4aa398aa5aca5b842fe0b12e23d17a3588
Bruno dos Santos Silva <142647702+B4Hive@users.noreply.github.com>
2025-01-28 19:25:00 +0000

    Added CLI.java; Imported to repl.it;

--- src/main/java/br/ufjf/b4hive/Megachile.java
+++ src/main/java/br/ufjf/b4hive/Megachile.java
@@ -1,7 +1,9 @@
 package br.ufjf.b4hive;
+import br.ufjf.b4hive.controller.CLI;
 
 public class Megachile {
     public static void main(String[] args) {
+        CLI.clear();
         System.out.println("Megachile");
     }
 }
